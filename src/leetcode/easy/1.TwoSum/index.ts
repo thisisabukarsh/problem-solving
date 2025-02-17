329fd370a004a4ee60ec891d3aeae620975d84be
thisisabukarsh <155810138+thisisabukarsh@users.noreply.github.com>
2025-02-17 09:36:25 +0300

    first q done

---
+++ src/leetcode/easy/1.TwoSum/index.ts
@@ -0,0 +1,17 @@
+const twoSum = (nums: number[], target: number): number[] => {
+  const map = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement)!, i];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+};
+
+// Test cases
+
+console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+console.log(twoSum([3, 2, 4], 6)); // [1, 2]
+console.log(twoSum([3, 3], 6)); // [0, 1]
